c27f1c54eb18f6e98ed4b7c46ef2356bd8d960d3
Sergey Pervak <pervak102kaf@mail.ru>
2024-09-27 17:49:22 +0300

    Правка шаблонов для авторизации

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -6,8 +6,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
+    # Подключаем urls.py приложения для работы с пользователями.
+    path('auth/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
